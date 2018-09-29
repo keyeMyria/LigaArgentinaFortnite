8960bbabf2d1f4e0573cc1520bf2878175035dc3
quiromaniac <mmquiroga10@gmail.com>
2018-09-29 02:37:32 -0300

    cambio a duo

--- sistema/models.py
+++ sistema/models.py
@@ -37,8 +37,11 @@ class Perfil(models.Model):
     partidas_2 = models.IntegerField(blank=True, default=0)
     twitch_2 = models.CharField(max_length=20, blank=True, default='')
 #GENERAL
+    partidas_totales = models.CharField(max_length=10, blank=True, default=0)
     kills_totales = models.CharField(max_length=10, blank=True, default=0)
     wins_totales = models.CharField(max_length=10, blank=True, default=0)
+    kills_liga = models.CharField(max_length=10, blank=True, default=0)
+    partidas_liga = models.CharField(max_length=10, blank=True, default=0)
     kd = models.CharField(max_length=4, blank=True, default=0)
     puntos = models.IntegerField(blank=True, default=0)
     general = models.IntegerField(blank=True, default=0)

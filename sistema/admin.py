be8fbe10e5954d3cfb50974bee55a6b5546fd467
quiromaniac <mmquiroga10@gmail.com>
2018-09-29 04:11:58 -0300

    cambio a duo

--- sistema/admin.py
+++ sistema/admin.py
@@ -156,7 +156,7 @@ def finalizar_torneo(modeladmin, request, queryset):
             km = postkills_liga / postpartidas_liga
             km = Decimal(km)
             km = round(km,2)
-        Perfil.objects.filter(user__username=u1).update(puntos=puntos, wins_totales=wins_totales, kills_totales=kills_totales, kd=km3, partidas_liga=postpartidas_liga, kills_liga=postkills_liga, general=nuevogeneral)
+        Perfil.objects.filter(user__username=u1).update(puntos=puntos, wins_totales=wins_totales, kills_totales=kills_totales, kd=km, partidas_liga=postpartidas_liga, kills_liga=postkills_liga, general=nuevogeneral)
 finalizar_torneo.short_description = "FINALIZAR TORNEO"
 
 class UserAdmin(BaseUserAdmin):

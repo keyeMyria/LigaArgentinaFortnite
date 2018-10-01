dce17b0546a2a2580f8eb6c5f4107a77b00ce989
quiromaniac <mmquiroga10@gmail.com>
2018-10-01 13:13:24 -0300

    cambio a duo

--- sistema/admin.py
+++ sistema/admin.py
@@ -173,8 +173,8 @@ finalizar_torneo.short_description = "FINALIZAR TORNEO"
 
 
 class UserAdmin(BaseUserAdmin):
-    ordering = ('date_joined')
-    #list_display = ('username', 'first_name', 'perfil__equipo', 'last_name', 'perfil__VERIFICACION_2')
+    #ordering = ('date_joined')
+    list_display = ('username', 'first_name', 'last_name', 'emailaddress', 'date_joined')
     inlines = [PerfilInline]
     actions = [resetear_torneo, resetear_todo, mail_comienzo_torneo, comenzar_torneo, finalizar_torneo]
 

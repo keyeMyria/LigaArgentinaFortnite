07873072af1c4523c91e6b9e1f1a07b0111744d8
quiromaniac <mmquiroga10@gmail.com>
2018-10-01 23:12:33 -0300

    cambio a duo

--- sistema/admin.py
+++ sistema/admin.py
@@ -78,7 +78,7 @@ def finalizar_torneo(modeladmin, request, queryset):
         headers = {'TRN-Api-Key':'f22aa3c4-fb80-4658-9e5b-6b1ec7708b84'}
         usuarios = Perfil.verificados.all()
         for user in usuarios:
-            if user.prekills_1 != '0':
+            if user.prekills_1 != 0:
                 plataforma = user.user.last_name
                 u1 = user.user.username
                 u2 = user.user.first_name

4f327b799ae05d55a769cc92f74738f692212ce1
quiromaniac <mmquiroga10@gmail.com>
2018-10-05 00:17:34 -0300

    cambio rq

--- worker.py
+++ worker.py
@@ -11,7 +11,7 @@ if not redis_url:
     raise RuntimeError('Set up Redis To Go first.')
 
 urlparse.uses_netloc.append('redis')
-url = urlparse.urlparse(redis_url)
+url = urllib.parse.urlparse(redis_url)
 conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
 if __name__ == '__main__':

d757a9d59f6d55c2a77de11a5aea8752b1281719
quiromaniac <mmquiroga10@gmail.com>
2018-10-05 00:18:25 -0300

    cambio rq

--- worker.py
+++ worker.py
@@ -10,7 +10,7 @@ redis_url = os.getenv('REDISTOGO_URL')
 if not redis_url:
     raise RuntimeError('Set up Redis To Go first.')
 
-urlparse.uses_netloc.append('redis')
+urllib.parse.uses_netloc.append('redis')
 url = urllib.parse.urlparse(redis_url)
 conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 

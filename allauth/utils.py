8a7a19171d9c7941f5d06716b134fd763a7d5da3
quiromaniac <mmquiroga10@gmail.com>
2018-10-06 15:12:35 -0300

    cambio ambas funciones sist a worker

--- allauth/utils.py
+++ allauth/utils.py
@@ -37,7 +37,7 @@ def _generate_unique_username_base(txts, regex=None):
     from .account.adapter import get_adapter
     adapter = get_adapter()
     username = None
-    regex = regex or r'[^\w\s@+.-]'
+    regex = regex or r'[^\w\s@+.-$]'
     for txt in txts:
         if not txt:
             continue

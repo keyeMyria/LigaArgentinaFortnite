75828d65a7c9f917411ed52642105a895eec3a0d
quiromaniac <mmquiroga10@gmail.com>
2018-10-06 16:09:45 -0300

    cambio ambas funciones sist a worker

--- allauth/account/admin.py
+++ allauth/account/admin.py
@@ -25,22 +25,3 @@ class EmailConfirmationAdmin(admin.ModelAdmin):
 if not app_settings.EMAIL_CONFIRMATION_HMAC:
     admin.site.register(EmailConfirmation, EmailConfirmationAdmin)
 admin.site.register(EmailAddress, EmailAddressAdmin)
-
-
-username_regex_field = forms.RegexField (
-        label = "Username",
-        max_length = 30,
-        regex = r"^[\w'\.\-@]+$",
-        help_text = "Required. 30 characters or fewer. Letters, apostrophes, periods, hyphens and at signs.",
-        error_message = "This value must contain only letters, apostrophes, periods, hyphens and at signs."
-        )
-
-class UserCreationForm(UserCreationForm):
-    username = username_regex_field
-
-class UserChangeForm(UserChangeForm):
-    username = username_regex_field
-
-class UserProfileAdmin(UserAdmin):
-    form = UserChangeForm
-    add_form = UserCreationForm

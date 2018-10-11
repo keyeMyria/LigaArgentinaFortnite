ec40b69f91acee0dc6f4f8f0a5ee46df7ccee38b
quiromaniac <mmquiroga10@gmail.com>
2018-10-11 16:03:43 -0300

    up

--- sistema/utils.py
+++ sistema/utils.py
@@ -14,7 +14,7 @@ from django.conf import settings
 
 def send_html_email(to_list, subject, template_name, context, sender=settings.DEFAULT_FROM_EMAIL):
     msg_html = render_to_string(template_name, context)
-    msg = EmailMessage(subject=subject, body=msg_html, from_email=sender, bcc=to_list, fail_silently=True)
+    msg = EmailMessage(subject=subject, body=msg_html, from_email=sender, bcc=to_list)
     msg.content_subtype = "html"  # Main content is now text/html
     return msg.send()
 

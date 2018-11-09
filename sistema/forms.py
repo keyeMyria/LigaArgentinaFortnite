f6b82699e8a67bfd3f0be1c50b2fbd56ad3af214
quiromaniac <mmquiroga10@gmail.com>
2018-11-09 16:43:33 -0300

    termino torneo

--- sistema/forms.py
+++ sistema/forms.py
@@ -14,7 +14,7 @@ class SignupForm(forms.Form):
     equipo = forms.CharField(max_length=30, label='Nombre del Equipo', required=True, widget=forms.TextInput(attrs={ 'class': 'input' ,'placeholder':('Equipo'),'autofocus': 'autofocus'}))
     twitch_1 = forms.CharField(max_length=30, label='Twitch del primer participante (no obligatorio)', required=False, widget=forms.TextInput(attrs={ 'class': 'input' ,'placeholder':('Usuario de Twitch'),'autofocus': 'autofocus'}))
     twitch_2 = forms.CharField(max_length=30, label='Twitch del segundo participante (no obligatorio)', required=False, widget=forms.TextInput(attrs={ 'class': 'input' ,'placeholder':('Usuario de Twitch'),'autofocus': 'autofocus'}))
-    telefono = forms.IntegerField(max_length=15, label='Telefono de contacto (obligatorio)', required=True, widget=forms.TextInput(attrs={ 'class': 'input' ,'placeholder':('Telefono'),'autofocus': 'autofocus'}))
+    telefono = forms.IntegerField(label='Telefono de contacto (obligatorio)', required=True, widget=forms.TextInput(attrs={ 'class': 'input' ,'placeholder':('Telefono'),'autofocus': 'autofocus'}))
 
 
     def signup(self, request, user):
@@ -23,5 +23,7 @@ class SignupForm(forms.Form):
         user.perfil.equipo = self.cleaned_data['equipo']
         user.perfil.twitch_1 = self.cleaned_data['twitch_1']
         user.perfil.twitch_2 = self.cleaned_data['twitch_2']
+        user.perfil.black_pan = black_pan
+        user.perfil.telefono = self.cleaned_data['telefono']
         user.save()
         #user.Perfil.equipo = self.cleaned_data['equipo']

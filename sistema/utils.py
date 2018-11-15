04f316c0337874521ba202a463360857b579b9c6
quiromaniac <mmquiroga10@gmail.com>
2018-11-14 21:52:10 -0300

    prueba graph

--- sistema/utils.py
+++ sistema/utils.py
@@ -329,7 +329,7 @@ def id_rq():
         query_u2 = query1 + '"' + u2 + '"' + query3
         ID1 = run_query(query_u1) # Execute the query
         ID2 = run_query(query_u2) # Execute the query
-        if 'data' in ID1.keys() and 'data' in ID2.keys():
+        if ID1 != "{'data': {'players': {'results': []}}}" and ID2 != "{'data': {'players': {'results': []}}}":
             if plataforma == 'psn':
                 ID1 = ID1["data"]["players"]["results"][0]['player']['playerId']
                 ID2 = ID2["data"]["players"]["results"][0]['player']['playerId']

699b3401f51ffc5ae3f7124b06f2d7a2143e9375
quiromaniac <mmquiroga10@gmail.com>
2018-11-16 17:41:23 -0300

    prueba OAUTH

--- sistema/utils.py
+++ sistema/utils.py
@@ -92,6 +92,7 @@ def id_rq():
                             ID1 = ID1["data"]["players"]["results"][-3]['player']['playerId']
                             Perfil.objects.filter(user__username=cuenta).update(id1=ID1)
 
+
                 ID_test2 = ID2["data"]["players"]["results"][-1]['player']
                 if ID_test2:
                     ID2 = ID2["data"]["players"]["results"][-1]['player']['playerId']

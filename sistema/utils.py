5bfa002faef53eb301025aa361b1c091f4df44d5
quiromaniac <mmquiroga10@gmail.com>
2018-11-16 02:27:42 -0300

    prueba OAUTH

--- sistema/utils.py
+++ sistema/utils.py
@@ -272,52 +272,82 @@ def finalizar_torneo_rq():
                                     send_html_email(emails, subject='EL TORNEO ACABA DE FINALIZAR! MIRA TUS RESULTADOS!', template_name='sistema/email/finalizar.html', context=context, sender="ligafortnitearg@gmail.com")
 
 def comenzar_torneo_prueba_rq():
-    URL = "https://api.fortnitetracker.com/v1/profile/"
-    headers = {'TRN-Api-Key':'f22aa3c4-fb80-4658-9e5b-6b1ec7708b84'}
     usuarios = Perfil.verificados.order_by('user__date_joined')
-    #Perfil.objects.filter(user__last_name='pc', VERIFICACION_2=True).order_by('-puntos')
+    r = requests.post(url = API_ENDPOINT, headers=headers_token, data=data)
+    hola = r.json()
+    token = hola['access_token']
+    bearer = 'Bearer ' + token
+    headers = {"Authorization": bearer ,"Scout-App": "ae45e214-016c-421c-aef1-35aaa1fe1201"}
+    def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
+        request = requests.post('https://api.scoutsdk.com/graph', json={'query': query}, headers=headers)
+        if request.status_code == 200:
+            return request.json()
+        else:
+            raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
     for user in usuarios:
         if user.prekills_1 == 0:
             equipo = user.equipo
-            plataforma = user.user.last_name
             cuenta = user.user.username
             cuenta2 = user.user.first_name
             u1 = user.user.username
             u2 = user.user.first_name
-            u1 = u1.replace(" ", "%20")
-            u2 = u2.replace(" ", "%20")
-            url1 = URL + plataforma + '/' + u1
-            url2 = URL + plataforma + '/' + u2
-            # MAIL COMIENZO TORNEO
-            # emails = [user.user.email]
-            # context = {
-            #     'equipo': equipo,
-            #     'u1': cuenta,
-            #     'u2': cuenta2
-            # }
-            # send_html_email(emails, subject='COMIENZA EL TORNEO! A JUGAR!', template_name='sistema/email/comenzar.html', context=context, sender="ligafortnitearg@gmail.com")
-            # API REQUESTS
-            respuesta_1 = requests.get(url1, headers=headers)
-            time.sleep(2)
-            resultado_1 = respuesta_1.json()
-            respuesta_2 = requests.get(url2, headers=headers)
-            time.sleep(2)
-            resultado_2 = respuesta_2.json()
-            if 'stats' in resultado_1.keys():
-                resultado_1 = respuesta_1.json()['stats']
-                if 'p10' in resultado_1.keys():
-                    prewins_1 = respuesta_1.json()['stats']['p10']['top1']['value']
-                    prekills_1 = respuesta_1.json()['stats']['p10']['kills']['value']
-                    pretop5_1 = respuesta_1.json()['stats']['p10']['top5']['value']
-                    prepartidas_1 = respuesta_1.json()['stats']['p10']['matches']['value']
-                    if 'stats' in resultado_2.keys():
-                        resultado_2 = respuesta_2.json()['stats']
-                        if 'p10' in resultado_2.keys():
-                            prewins_2 = respuesta_2.json()['stats']['p10']['top1']['value']
-                            prekills_2 = respuesta_2.json()['stats']['p10']['kills']['value']
-                            #top2 = respuesta_2.json()['stats']['p10']['top5']['value']
-                            prepartidas_2 = respuesta_2.json()['stats']['p10']['matches']['value']
-                            Perfil.objects.filter(user__username=cuenta).update(prekills_1=prekills_1, prewins_1=prewins_1, prepartidas_1=prepartidas_1, pretop5_1=pretop5_1, prekills_2=prekills_2, prewins_2=prewins_2, prepartidas_2=prepartidas_2)
+            ID1 = user.id1
+            ID2 = user.id2
+
+            query_stats1 = """
+            {
+              player(title: "fortnite", id: """
+
+            query_stats2 = """, segment: "p10.br.m0.alltime") {
+                id
+                metadata {
+                  key
+                  name
+                  value
+                  displayValue
+                }
+                stats {
+                  metadata {
+                    key
+                    name
+                    isReversed
+                  }
+                  value
+                  displayValue
+                }
+                segments {
+                  metadata {
+                    key
+                    name
+                    value
+                    displayValue
+                  }
+                  stats {
+                    metadata {
+                      key
+                      name
+                      isReversed
+                    }
+                    value
+                    displayValue
+                  }
+                }
+              }
+            }
+            """
+            query_u1 = query_stats1 + '"' + ID1 + '"' + query_stats2
+            query_u2 = query_stats1 + '"' + ID2 + '"' + query_stats2
+            stats1 = run_query(query_u1)
+            stats2 = run_query(query_u2)
+            prekills_1 = stats1['data']['player']['segments'][0]['stats'][0]['value']
+            prewins_1 = stats1['data']['player']['segments'][0]['stats'][3]['value']
+            pretop5_1 = stats1['data']['player']['stats'][5]['value']
+            prepartidas_1 = stats1['data']['player']['segments'][0]['stats'][2]['value']
+            prekills_2 = stats2['data']['player']['segments'][0]['stats'][0]['value']
+            prewins_2 = stats2['data']['player']['segments'][0]['stats'][3]['value']
+            prepartidas_2 = stats2['data']['player']['segments'][0]['stats'][2]['value']
+            Perfil.objects.filter(user__username=cuenta).update(prekills_1=prekills_1, prewins_1=prewins_1, prepartidas_1=prepartidas_1, pretop5_1=pretop5_1, prekills_2=prekills_2, prewins_2=prewins_2, prepartidas_2=prepartidas_2)
+
 
 def mail_comienzo_torneo_rq():
     usuarios = Perfil.verificados.order_by('user__date_joined')
